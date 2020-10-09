88a7fee54b3265755970ad1efe54adcbcef876d4
Rafael Pastrana <arpj@princeton.edu>
2020-10-08 19:16:32 -0500

    Docstringed node and edge

--- src/compas_cem/elements/edge.py
+++ src/compas_cem/elements/edge.py
@@ -13,6 +13,21 @@ class Edge(object):
     @classmethod
     def from_line(cls, line, *args, **kwargs):
         """
+        Create an edge from a line described by two xyz coordinates.
+
+        Parameters
+        ----------
+        line : ``tuple`` or ``list``
+            The xyz coordinates of the line's two end points.
+        *args : ``tuple``
+            Additional arguments.
+        **kwargs : ``dict``
+            Extra keyword arguments.
+        
+        Returns
+        -------
+        edge : ``Edge``
+            An edge object.
         """
         edge = cls(line[0], line[1], *args, **kwargs)
         return edge
@@ -20,12 +35,33 @@ class Edge(object):
     @classmethod
     def from_rhino_line(cls, rhino_line, *args, **kwargs):
         """
+        Create an edge from a rhino line.
+
+        Parameters
+        ----------
+        rhino_line : ``Rhino.Geometry.Line``
+            A rhino line.
+        *args : ``tuple``
+            Additional arguments.
+        **kwargs : ``dict``
+            Extra keyword arguments.
+        
+        Returns
+        -------
+        edge : ``Edge``
+            An edge object.
         """
         line = RhinoLine.from_geometry(rhino_line).to_compas()
         return cls.from_line(line, *args, **kwargs)
 
     def __iter__(self):
         """
+        Iterates over the start and end nodes of an edge.
+
+        Yields
+        ------
+        key : ``int``
+            The next node key.
         """
         for node in (self.u, self.v):
             yield node
--- src/compas_cem/elements/node.py
+++ src/compas_cem/elements/node.py
@@ -11,14 +11,44 @@ class Node(object):
         self.attributes = {}
 
     @classmethod
-    def from_point(cls, point):
+    def from_point(cls, point, *args, **kwargs):
         """
+        Create a node from a point described by its xyz coordinates.
+
+        Parameters
+        ----------
+        point : ``tuple`` or ``list``
+            The xyz coordinates of the point.
+        *args : ``tuple``
+            Additional arguments.
+        **kwargs : ``dict``
+            Extra keyword arguments.
+        
+        Returns
+        -------
+        node : ``Node``
+            A node object.
         """
         return cls(xyz=point)
     
     @classmethod
-    def from_rhino_point(cls, rhino_point):
+    def from_rhino_point(cls, rhino_point, *args, **kwargs):
         """
+        Create a node from a rhino point.
+
+        Parameters
+        ----------
+        rhino_point : ``Rhino.Geometry.Point3d``
+            A rhino point.
+        *args : ``tuple``
+            Additional arguments.
+        **kwargs : ``dict``
+            Extra keyword arguments.
+        
+        Returns
+        -------
+        node : ``Node``
+            A node object.
         """
         point = RhinoPoint.from_geometry(rhino_point).to_compas()
         return cls.from_point(point)
