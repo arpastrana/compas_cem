6f2b4dc45b3592b80eb5c0620b799d2faa5804de
Rafael Pastrana <arpj@princeton.edu>
2021-07-04 18:15:21 -0500

    Bump version to 0.1.5

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ optional_requirements = {}
 
 setup(
     name="compas_cem",
-    version="0.1.4",
+    version="0.1.5",
     description="Constrained form-finding meets automatic differentiation.",
     long_description=long_description,
     long_description_content_type="text/markdown",
