9f6a8e1246da0ab96b5f41bb2f15fb3fc3ec3bc4
Rafael Pastrana <arpj@princeton.edu>
2021-07-05 13:39:40 -0500

    Matched release.yml action to COMPAS'

--- src/compas_cem/ghpython/uninstall.py
+++ src/compas_cem/ghpython/uninstall.py
@@ -11,20 +11,13 @@ from compas_ghpython.components import uninstall_userobjects
 
 @compas.plugins.plugin(category='install')
 def after_rhino_uninstall(uninstalled_packages):
-    print(1)
     if 'compas_cem' not in uninstalled_packages:
-        print(2)
         return []
-    print(3)
     srcdir = os.path.join(os.path.dirname(__file__), 'components', 'ghuser')
     userobjects = [os.path.basename(ghuser) for ghuser in glob.glob(os.path.join(srcdir, '*.ghuser'))]
     uninstalled_objects = uninstall_userobjects(userobjects)
-    print('srcdir', srcdir)
-    print('userobjects', userobjects)
-    print('uninstalled_objects', uninstalled_objects)
 
     uninstall_errors = [uo[0] for uo in uninstalled_objects if not uo[1]]
     error_msg = '' if not uninstall_errors else 'and {} failed to uninstall'.format(len(uninstall_errors))
-    print(4)
 
     return [('compas_cem', 'Uninstalled {} GH User Objects {}'.format(len(uninstalled_objects), error_msg), True)]
