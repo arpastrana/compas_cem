ff9f1b21774503d009b8a2a4ae599624603c922e
Rafael Pastrana <arpj@princeton.edu>
2021-09-06 19:18:28 -0400

    Fused `AssembleTopologyDiagram` and `BuildTrails` in ghplugin

--- src/compas_cem/ghpython/components/CEM_TopologyDiagram/code.py
+++ src/compas_cem/ghpython/components/CEM_TopologyDiagram/code.py
@@ -1,19 +1,16 @@
 """
-Build a topology diagram.
+Assemble a topology diagram.
 """
 from ghpythonlib.componentbase import executingcomponent as component
 
 from compas_cem.diagrams import TopologyDiagram
 
 
-class BuildTopologyDiagramComponent(component):
-    def RunScript(self, nodes, trail_edges, deviation_edges, loads, supports):
+class AssembleTopologyDiagramComponent(component):
+    def RunScript(self, trail_edges, deviation_edges, loads, supports, add_auxiliary_trails):
 
+        add_auxiliary_trails = add_auxiliary_trails or False
         topology = TopologyDiagram()
-
-        if nodes:
-            for node in nodes:
-                topology.add_node(node)
 
         if trail_edges:
             for trail_edge in trail_edges:
@@ -30,5 +27,8 @@ class BuildTopologyDiagramComponent(component):
         if supports:
             for support in supports:
                 topology.add_support(support)
+
+        if trail_edges and supports:
+            topology.build_trails(add_auxiliary_trails)
 
         return topology
