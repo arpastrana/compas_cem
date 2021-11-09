47bfd9718f10aed71bc80caf826515d6b7c45054
Rafael Pastrana <arpj@princeton.edu>
2021-11-09 01:09:23 -0500

    Bump version to 0.2.1

--- src/compas_cem/__init__.py
+++ src/compas_cem/__init__.py
@@ -26,7 +26,7 @@ __author__ = ["Rafael Pastrana"]
 __copyright__ = "Copyright 2020 - Princeton University"
 __license__ = "MIT License"
 __email__ = "arpj@princeton.edu"
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 
 # Directories
