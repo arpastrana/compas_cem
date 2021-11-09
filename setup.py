b57b64dc22f8d9228387d94250aa3891ee1a9626
Rafael Pastrana <arpj@princeton.edu>
2021-11-09 00:22:49 -0500

    Bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ optional_requirements = {}
 
 setup(
     name="compas_cem",
-    version="0.1.15",
+    version="0.2.0",
     description="The Combinatorial Equilibrium Modeling framework for COMPAS",
     long_description=long_description,
     long_description_content_type="text/markdown",
