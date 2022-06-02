b964db277bb16bd21f0a63de1adbcc421bf7ba19
Rafael Pastrana <arpj@princeton.edu>
2022-06-02 13:13:13 -0400

    [Viewers] Added the `compas_cem.viewers` folder

---
+++ src/compas_cem/viewers/__init__.py
@@ -0,0 +1,27 @@
+"""
+compas_cem.viewers
+****************************
+
+.. currentmodule:: compas_cem.viewers
+
+
+Classes
+=======
+
+.. autosummary::
+    :toctree: generated/
+    :nosignatures:
+
+    FormObject
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+
+# from .<module> import *
+from .formobject import *  # noqa F403
+# from .topologyartist import *  # noqa F403
+
+__all__ = [name for name in dir() if not name.startswith('_')]
