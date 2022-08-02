c927f4eb721316ba2e3e693c3f19e93f41068948
Rafael Pastrana <arpj@princeton.edu>
2022-08-02 19:22:03 -0400

    [Opt] Finite differences copies `x` only once per evaluation

--- src/compas_cem/optimization/grad.py
+++ src/compas_cem/optimization/grad.py
@@ -33,20 +33,22 @@ def grad_finite_differences(x, grad, x_func, step_size, **kwargs):
     Approximates the gradient of a blackbox function using finite differences.
     """
     fx0 = x_func(x)
+    # NOTE: Make an editable copy of x because NLOpt makes x a read-only vector
+    _x = np.copy(x)
 
-    # TODO: Can we vectorize this loop with numpy
-    for i in range(len(x)):  # vectorize with numpy?
+    for i in range(len(x)):
 
-        # TODO: For fairness, can we get along without copying vector x?
-        _x = np.copy(x)
+        _xi = _x[i]
         _x[i] += step_size
 
-        fx1 = x_func(_x)  # bottleneck
+        fx1 = x_func(_x)
 
         delta_fx = (fx1 - fx0) / step_size
         grad[i] = delta_fx
+        _x[i] = _xi
 
     return grad
+
 
 # ------------------------------------------------------------------------------
 # Main
