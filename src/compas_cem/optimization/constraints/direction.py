f039b8292410d5f33105e3556b6218ed5a0693d4
Rafael Pastrana <arpj@princeton.edu>
2022-11-17 10:39:11 -0500

    [Constraints] `EdgeDirectionConstraint` aligns target to reference

--- src/compas_cem/optimization/constraints/direction.py
+++ src/compas_cem/optimization/constraints/direction.py
@@ -1,3 +1,4 @@
+from compas.geometry import dot_vectors
 from compas.geometry import normalize_vector
 from compas.geometry import subtract_vectors
 from compas.geometry import length_vector_sqrd
@@ -12,28 +13,56 @@ __all__ = ["EdgeDirectionConstraint"]
 class EdgeDirectionConstraint(VectorConstraint):
     """
     Align the direction of a trail or a deviation edge with a target vector.
+
+    Note that the ordering of the nodes of the edge to constrain matters.
+    The reference direction of the edge is a vector pointing from its starting node (u),
+    towards its end node (v).
     """
     def __init__(self, edge=None, vector=None, weight=1.0):
         super(EdgeDirectionConstraint, self).__init__(edge, vector, weight)
 
     def target(self, reference):
         """
-        The target vector.
+        The target, unit-length vector.
         """
-        return normalize_vector(self._target)
+        target = normalize_vector(self._target)
+        return self._aligned_vector(target, reference)
 
     def reference(self, data):
         """
-        The edge direction.
+        The unitized edge direction.
         """
-        # cosine_similarity = dot_vectors(u, v) / (length_vector(u) * length_vector(v))
-        # 1 - fabs(cosine_similarity)
-        u, v = self.key()
-        vector = subtract_vectors(data["node_xyz"][v], data["node_xyz"][u])
+        vector = self._vector_two_points(self.key(), data)
+        return self._unitized_vector(vector)
+
+    @staticmethod
+    def _vector_two_points(edge, data):
+        """
+        Create a vector from the XYZ coordinates of two nodes.
+        """
+        u, v = edge
+        return subtract_vectors(data["node_xyz"][v], data["node_xyz"][u])
+
+    @staticmethod
+    def _unitized_vector(vector):
+        """
+        Scale a copy of a vector such that its length is equal to one.
+        """
         return scale_vector(vector, 1.0 / (length_vector_sqrd(vector) ** 0.5))
+
+    @staticmethod
+    def _aligned_vector(vector, vector_ref):
+        """
+        Align a vector to another such that their dot product is non-negative.
+        """
+        if dot_vectors(vector, vector_ref) < 0.0:
+            return scale_vector(vector, -1.0)
+        return vector
 
 
 if __name__ == "__main__":
+
+    from math import fabs
 
     from compas.geometry import Translation
 
@@ -72,7 +101,7 @@ if __name__ == "__main__":
     # Add edges
     # ------------------------------------------------------------------------------
 
-    topology.add_edge(TrailEdge(0, 1, length=-1.0))
+    topology.add_edge(TrailEdge(1, 0, length=-1.0))
     topology.add_edge(DeviationEdge(1, 2, force=-1.0))
     topology.add_edge(TrailEdge(2, 3, length=-1.0))
 
@@ -112,9 +141,10 @@ if __name__ == "__main__":
     # Define constraints
     # ------------------------------------------------------------------------------
 
-    vector = [-2.0, -1.0, 0.0]
-    constraint = EdgeDirectionConstraint((0, 1), vector)
-    opt.add_constraint(constraint)
+    vector = [0.0, -2.0, 0.0]
+    for edge in topology.trail_edges():
+        constraint = EdgeDirectionConstraint(edge, vector)
+        opt.add_constraint(constraint)
 
     # ------------------------------------------------------------------------------
     # Define optimization parameters
@@ -127,10 +157,17 @@ if __name__ == "__main__":
     # Optimization
     # ------------------------------------------------------------------------------
 
-    # optimize
     form_opt = opt.solve(topology=topology,
                          algorithm="SLSQP",
                          verbose=True)
+
+    # ------------------------------------------------------------------------------
+    # Test
+    # ------------------------------------------------------------------------------
+
+    for edge in topology.deviation_edges():
+        force = fabs(topology.edge_force(edge))
+        assert force <= 1e-3, f"Absolute edge force: {force}"
 
     # ------------------------------------------------------------------------------
     # Plot results
