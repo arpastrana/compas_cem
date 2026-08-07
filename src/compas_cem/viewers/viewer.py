4b2ce7037680a94efc76b1d30ffed30abb6b3c75
Rafael Pastrana <arpj@princeton.edu>
2026-08-07 12:36:38 -0400

    [Viewers] Ported the diagram objects to compas_viewer scene objects

--- src/compas_cem/viewers/viewer.py
+++ src/compas_cem/viewers/viewer.py
@@ -1,43 +1,14 @@
-from compas_view2.app import App
-
-from compas_cem.diagrams import FormDiagram
-from compas_cem.diagrams import TopologyDiagram
+from compas_viewer import Viewer
 
 __all__ = ["Viewer"]
 
 
-class Viewer(App):
+class Viewer(Viewer):
     """
-    A thin wrapper on the :class:`compas_view2.app.App`.
+    A thin wrapper on the `compas_viewer.Viewer`.
 
-    The intention is to facilitate some operations specific to CEM, such as
-    registering the diagram objects, and adding the viewer itself as an attribute
-    of the objects.
+    This object exists only for API consistency with `compas_cem.plotters.Plotter`.
     """
 
     def __init__(self, *args, **kwargs):
         super(Viewer, self).__init__(*args, **kwargs)
-
-    def add(self, data, **kwargs):
-        """
-        Add a COMPAS data object to the viewer.
-
-        It adds a viewer argument if the object is a :class:`compas_cem.diagrams.FormDiagram`
-        or a :class:`compas_cem.diagrams.TopologyDiagram`.
-
-        Parameters
-        ----------
-        data: :class:`compas.geometry.Primitive` | :class:`compas.geometry.Shape` | :class:`compas.datastructures.Datastructure`
-            A COMPAS data object.
-        **kwargs : dict, optional
-            Additional visualization options.
-
-        Returns
-        -------
-        view_data :class:`compas_view2.objects.Object`
-            A visualization object.
-        """
-        if isinstance(data, (FormDiagram, TopologyDiagram)):
-            kwargs = kwargs or {}
-            kwargs["viewer"] = self
-        return super(Viewer, self).add(data, **kwargs)
